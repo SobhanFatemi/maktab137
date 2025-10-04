a9966fe3355f1ce7769594d4003322edbed9bcb7
Sobhan Fatemi <sobhanfatemi55@gmail.com>
2025-10-04 22:33:36 +0330

    couple of changes

--- week01/hw/hw1-2.py
+++ week01/hw/hw1-2.py
@@ -1,18 +1,14 @@
-user_input: list = list(input("Please enter your name: "))
+a = int(input("Please enter your number: "))
 
-i: int = 0
+i: int = 1
+x: int = 0
 
-while i < len(user_input):
-    if user_input[i] == " ":
-        del user_input[i]
-
-    if user_input[i] == "a" or  user_input[i] == "e" or  user_input[i] == "i" or  user_input[i] == "o" or  user_input[i] == "u" or  user_input[i] == "A" or  user_input[i] == "E" or  user_input[i] == "I" or  user_input[i] == "O" or  user_input[i] == "U":
-        user_input[i] = "."
+while i < a:
+    if a % i == 0:
+        x += i
     i += 1
 
-user_input.reverse()
-
-# found this line in stackoverflow
-user_input_str: str = ''.join(user_input)
-
-print(user_input_str)
+if x == a:
+    print("YES")
+else:
+    print("NO")
